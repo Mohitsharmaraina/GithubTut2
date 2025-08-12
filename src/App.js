7349be75891f31c5dad9286d716a34fd5e67a37d
MOHITK_KTPL <mohitraina833@gmail.com>
2025-08-12 15:00:22 +0530

    main commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Boxes from "./components/box";
+function App() {
+  return (
+    <div className="App">
+      <Boxes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/box.js
@@ -0,0 +1,22 @@
+const Boxes = () => {
+  return (
+    <>
+      <div
+        style={{ height: 100, width: 200, border: "2px solid ", margin: 10 }}
+      >
+        White box
+      </div>
+      <div
+        style={{ height: 100, width: 200, border: "2px solid ", margin: 10 }}
+      >
+        White box
+      </div>
+      <div
+        style={{ height: 100, width: 200, border: "2px solid ", margin: 10 }}
+      >
+        White box
+      </div>
+    </>
+  );
+};
+export default Boxes;
